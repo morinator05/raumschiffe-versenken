69b25c61e451b4fd73b9bc50d8d8ac27b6a1880d
Moritz <roemoben@gmail.com>
2023-05-09 00:22:09 +0200

    init commit

---
+++ src/Game.java
@@ -0,0 +1,91 @@
+public class Game {
+
+    //global variables
+    private char[][] field = new char[10][10];
+    private char[][] testField = {{'w', 's', 's', 'w', 'w', 'w', 'w', 'w', 'w', 'w',},
+            {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w',},
+            {'w', 'w', 's', 's', 's', 'w', 'w', 'w', 'w', 'w',},
+            {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w',},
+            {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w',},
+            {'w', 's', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w',},
+            {'w', 's', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w',},
+            {'w', 'w', 's', 's', 'w', 'w', 'w', 'w', 'w', 'w',},
+            {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w',},
+            {'w', 'w', 'w', 'w', 'w', 's', 's', 's', 's', 'w',}};
+    private int[] lengthOfShipList = {2, 3, 4, 5}; //small, small_medium, large_medium, large
+    public Game() {
+        checkPlacement(9, 0, false, 'd', testField);
+        printField(testField);
+    }
+
+    public void setFieldAllWater(char field[][]) {
+        for(int i = 0; i < field.length; i++) {
+            for(int j = 0; j < field[0].length; j++) {
+                field[j][i] = 'w';
+            }
+        }
+    }
+
+    public void printField(char field[][]) {
+        System.out.println("debug: current field");
+        for(int i = 0; i < field.length; i++) {
+            for(int j = 0; j < field[0].length; j++) {
+                System.out.print(field[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    //check if entered ship placement is valid and does not confront with other ships
+    public int checkPlacement(int x, int y, boolean rotation, char type, char[][] field) {
+        //convert ship type to length
+        int lengthOfShip = getLengthFromType(type);
+
+        //set start and end coords and checks if ship would be out of bounds
+        int startPosX = x - 1; int startPosY = y - 1;
+        int endPosX = 0; int endPosY = 0;
+        if(rotation == true) {
+            endPosX = startPosX + lengthOfShip + 1;
+            endPosY = startPosY + 2;
+            if((x + lengthOfShip > 10)) {System.out.println("error: invalid ship placement"); return -1;}
+        }
+        else{
+            endPosY = startPosY + lengthOfShip + 1;
+            endPosX = startPosX + 2;
+            if((y + lengthOfShip > 10)) {System.out.println("error: invalid ship placement"); return -1;}
+        }
+        if(endPosX > 9) {endPosX = 9;}
+        if(endPosY > 9) {endPosY = 9;}
+        if(startPosX < 0) {startPosX = 0;}
+        if(startPosY < 0) {startPosY = 0;}
+        System.out.println("debug: checking from (" + startPosX + "|" + startPosY + ") to (" + endPosX + "|" + endPosY + ")"); //for checking the start and end coords of the area that is checked
+
+        for(int currentPosY = startPosY; currentPosY <= endPosY; currentPosY++) {
+            for (int currentPosX = startPosX; currentPosX <= endPosX; currentPosX ++) {
+                //field[currentPosY][currentPosX] = '*';
+                if(field[currentPosY][currentPosX] != 'w') {
+                    System.out.println("error: invalid ship placement");
+                    //return -1 if placement is invalid
+                    return -1;
+                }
+            }
+        }
+        //return 0 if valid
+        return 0;
+    }
+
+    private int getLengthFromType(char type) {
+        int lengthOfShip = 0;
+        switch (type) {
+            case 'a': return lengthOfShipList[0];
+            case 'b': return lengthOfShipList[1];
+            case 'c': return lengthOfShipList[2];
+            case 'd': return lengthOfShipList[3];
+        }
+        System.out.println("error: could not get length of ship from type");
+        return -1;
+    }
+    public static void main(String[] args) {
+        Game game1 = new Game();
+    }
+}
