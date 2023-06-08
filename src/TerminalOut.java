783c4361f8bd5b25ec9205f4ffa9da96ba416f01
Moritz <roemoben@gmail.com>
2023-06-08 17:47:42 +0200

    addet class for terminal output

---
+++ src/TerminalOut.java
@@ -0,0 +1,22 @@
+public class TerminalOut {
+    
+
+    public TerminalOut() {}
+
+    //print console messages for the player to read (info etc.)
+    public void TerminalMessageOut(int message) {
+        
+    }
+
+    //print own and enemy(the one you see) field
+    public void printField(char[][] field0, char[][] field1) {
+        
+    }
+
+    //get player input for placing ship
+    public void playerInputPlaceShip() {
+
+    }
+
+
+}
