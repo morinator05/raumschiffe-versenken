8822ded2d6104d99e754d9fe42c24678e174b520
Moritz <roemoben@gmail.com>
2023-06-28 10:13:25 +0200

    add new features

--- src/TerminalOut.java
+++ src/TerminalOut.java
@@ -8,59 +8,51 @@ public class TerminalOut {
     private char waterSymbol = '~';//w ater texture
     private char hitSymbol = 'x'; //char if a ship was hit (x, )
     private char missSymbol = 'O';//miss
-    private String[][] terminalMessages = { {"check" },
-                                            {"test"},
-                                            {"kekW"},
+    private String[][] terminalMessages = { {"Your Field:", "Enemy Field:", "Input the X Coord:",       "Input the Y Coord:",      "Input the Rotation: ",  "Bad Input, try aggain:",               "Input Your Placement Parameters for the ship:",    "Input the Type:",          "No ships of this type remaining, try other:"           },
+                                            {"Dein Feld:",  "Gengner Feld", "Gib die X Position ein:",  "Gib die Y Position ein:", "Gib die Richtung ein:", "Ungültige Eingabe, versuche erneut:",  "Gib die Parameter für das Schiff ein:",            "Geben sie den Typ ein:",   "Keine Schiffe dieses Typs mehr, anderen Typ eingeben:" },
     };
 
-    public TerminalOut() {}
+    public TerminalOut() {
+    }
 
     //print console messages for the player to read (info etc.)
     public void TerminalMessageOut(int message) {
-        switch(message){
-            case 1 : {
+        switch (message) {
+            case 1: {
                 System.out.println("You Won!");
             }
-            case 2 :{
+            case 2: {
                 System.out.println("You Lost!");
             }
-            case 3 : {
+            case 3: {
                 System.out.println("Ship placement not allowed!");
             }
-            case 4 : {
+            case 4: {
                 System.out.println("Error!");
             }
-            case 5 : {
-                System.out.println("Winner Winner Chicken Dinner");  
+            case 5: {
+                System.out.println("Winner Winner Chicken Dinner");
             }
-            case 6 : {
+            case 6: {
                 System.out.println("Miss!");
             }
-            case 7 : {
+            case 7: {
                 System.out.println("Hit!");
             }
-            case 8 : {
+            case 8: {
                 System.out.println("Made by: ");
             }
-            case 9 : {
+            case 9: {
                 System.out.println("");
             }
         }
     }
 
     //interface for placing the ships //int[0] = x; int [1] = y
-    public int[] playerInputPlace(char[][] ownField) {
-        Scanner scan = new Scanner(System.in);
-        int[] coords = new int[2];
-
+    public void playerInputPlace(char[][] ownField) {
+        System.out.println(terminalMessages[lang][0]);
         printFieldWithNumbers(ownField);
-
-        System.out.println(terminalMessages[lang][0]);
-        coords[0] = scan.nextInt();
-        System.out.println(terminalMessages[lang][0]);
-        coords[1] = scan.nextInt();
-
-        return coords;
+        System.out.println(terminalMessages[lang][6]);
     }
 
     //interface while in game
@@ -76,23 +68,23 @@ public class TerminalOut {
     private void printFieldWithNumbers(char[][] field) {
         System.out.println("   1 2 3 4 5 6 7 8 9 10"); //print numbers from 1 to 10 at the top
 
-        for(int i = 0; i < field.length; i++) {
+        for (int i = 0; i < field.length; i++) {
 
             //print numbers from 1 to 10 vertically for each line
-            if(i < 9) { System.out.print(i + 1 + "  "); }
-            else { System.out.print(i + 1 + " "); }
+            if (i < 9) {
+                System.out.print(i + 1 + "  ");
+            } else {
+                System.out.print(i + 1 + " ");
+            }
 
-            for(int j = 0; j < field[0].length; j++) {
-                if(field[i][j] == 'w') {
+            for (int j = 0; j < field[0].length; j++) {
+                if (field[i][j] == 'w') {
                     System.out.print(waterSymbol + " ");
-                }
-                else if(field[i][j] == 's') {
+                } else if (field[i][j] == 's') {
                     System.out.print(shipSymbol + " ");
-                }
-                else if(field[i][j] == 'h') {
-                    System.out.print(hitSymbol+ " ");
-                }
-                else if(field[i][j] == 'm') {
+                } else if (field[i][j] == 'h') {
+                    System.out.print(hitSymbol + " ");
+                } else if (field[i][j] == 'm') {
                     System.out.print(missSymbol + " ");
                 }
             }
@@ -100,5 +92,52 @@ public class TerminalOut {
         }
     }
 
+    public int inputCoordX() {
+        System.out.println(terminalMessages[lang][2]);
+        Scanner scan = new Scanner(System.in);
+        return scan.nextInt() - 1;
+    }
 
+    public int inputCoordY() {
+        System.out.println(terminalMessages[lang][3]);
+        Scanner scan = new Scanner(System.in);
+        return scan.nextInt() - 1;
+    }
+
+    public boolean inputRotation() {
+        System.out.println(terminalMessages[lang][4]);
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            int temp = scan.nextInt();
+
+            if (temp == 1) {
+                return true;
+            } else if (temp == 2) {
+                return false;
+            } else {
+                System.out.println(terminalMessages[lang][5]);
+            }
+        }
+
+    }
+
+    public int inputType(int[] shipsRemaining) {
+        System.out.println(terminalMessages[lang][7]);
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            int temp = scan.nextInt();
+
+            if(shipsRemaining[temp - 1] != 0) {
+                System.out.println(terminalMessages[lang][8]);
+            }
+            else if (temp <= 4 && temp >= 1) {
+                return temp - 1; //-1 to fit the array for the type
+            }
+            else {
+                System.out.println(terminalMessages[lang][5]);
+            }
+        }
+    }
 }
